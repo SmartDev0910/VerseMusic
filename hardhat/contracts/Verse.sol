140785ec7d98b25fb394bca0b1e74643ccbbfdf0
SmartDev0910 <roryporter0910@gmail.com>
2023-03-05 23:05:09 -0500

    Load openzepplelin library

--- hardhat/contracts/Verse.sol
+++ hardhat/contracts/Verse.sol
@@ -1,11 +1,10 @@
-//SPDX-License-Identifier: UNLICENSED
-
-// Solidity files have to start with this pragma.
-// It will be used by the Solidity compiler to validate its version.
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
 contract Verse {
-    function addMinter() public {
-        
-    }
+    address public owner;
 }
